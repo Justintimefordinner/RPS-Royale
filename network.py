e3a1601ce4458214cc1fc40211291c7108d4b8bd
Wyvern27 <ppritchard2007@gmail.com>
2024-05-03 11:06:10 -0500

    fix

--- network.py
+++ network.py
@@ -5,9 +5,7 @@ class Network:
 
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.host = '172.16.102.100' # For this to work on your machine this must be equal to the ipv4 address of the machine running the server
-                                    # You can find this address by typing ipconfig in CMD and copying the ipv4 address. Again this must be the servers
-                                    # ipv4 address. This feild will be the same for all your clients.
+        self.host = '172.16.102.145'
         self.port = 1738
         self.addr = (self.host, self.port)
         self.id = self.connect()
