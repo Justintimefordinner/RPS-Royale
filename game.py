930cd19926a6e46fd77707bfbca322f207dfabfb
Justintimefordinner <justwright916@gmail.com>
2024-05-03 12:00:20 -0500

    Added Debug mode and updated movement

--- game.py
+++ game.py
@@ -7,15 +7,22 @@ class Camera:
         self.camera = pygame.Rect(0, 0, width, height)
         self.width = width
         self.height = height
+        self.lerp_speed = 0.1  # Adjust this value to change the speed of the camera movement
 
     def apply(self, entity):
-        return entity.rect.move(self.camera.topleft)
+        return pygame.Rect(entity.rect.x - self.camera.x, entity.rect.y - self.camera.y, entity.rect.width, entity.rect.height)
 
     def update(self, target):
-        x = -target.rect.centerx + int(self.width / 2)
-        y = -target.rect.centery + int(self.height / 2)
+        target_center_x = target.rect.centerx
+        target_center_y = target.rect.centery
 
-        self.camera = pygame.Rect(x, y, self.width, self.height)
+        # Linear interpolation (lerp) for smooth camera movement
+        self.camera.x += (target_center_x - self.camera.x - self.width // 2) * self.lerp_speed
+        self.camera.y += (target_center_y - self.camera.y - self.height // 2) * self.lerp_speed
+
+        # Limit scrolling to map size
+        self.camera.x = max(-(self.width // 2), min(self.camera.x, target.rect.width - self.width // 2))
+        self.camera.y = max(-(self.height // 2), min(self.camera.y, target.rect.height - self.height // 2))
 
 class Player():
     width = height = 50
@@ -32,14 +39,14 @@ class Player():
         self.speed = 0
         self.acceleration = 1
         self.max_speed = 10
+        self.glide_x = 0
+        self.glide_y = 0
     
     @property
     def rect(self):
         return pygame.Rect(self.x, self.y, self.width, self.height)
     
     def draw(self, g, camera):
-        self.surf = pygame.Surface((self.width, self.height))
-        self.surf.fill(self.color)
         g.blit(self.surf, camera.apply(self))
         
     def move(self, direction):
@@ -49,59 +56,80 @@ class Player():
             self.angle -= self.rotation_speed
         elif direction == 2:  # Move forward
             self.speed = min(self.speed + self.acceleration, self.max_speed)
-
+            self.glide_x = math.cos(math.radians(self.angle))
+            self.glide_y = math.sin(math.radians(self.angle))
+    
         # Update position based on speed and angle
-        self.x += math.cos(math.radians(self.angle)) * self.speed
-        self.y += math.sin(math.radians(self.angle)) * self.speed
-
+        self.x += self.glide_x * self.speed
+        self.y -= self.glide_y * self.speed  # Subtract to make the movement relative to the player's direction
+    
         # Deceleration
         self.speed *= 0.99
 
 
 class Game:
 
-    def __init__(self, w, h):
-        self.net = Network()
+    def __init__(self, w, h, debug=False):
+        self.debug = debug
+        if not self.debug:
+            self.net = Network()
+            self.players = [self.net.getPlayer()]
+        else:
+            self.players = [Player(100,100)]
+            
+        self.camera = Camera(500,500)
         self.width = w
         self.height = h
-        self.player = Player(50, 50)
-        self.player2 = Player(100,100)
-        self.canvas = Canvas(self.width, self.height, "Testing...")
-        self.camera = Camera(w, h)
+        # self.player = Player(50, 50)
+        # self.player2 = Player(100,100)
+        cell_size = 50
+        self.canvas = Canvas(self.width, self.height, cell_size, "Testing...")
 
     def run(self):
         clock = pygame.time.Clock()
         run = True
         while run:
+            
             clock.tick(60)
-            self.camera.update(self.player)
+            
+            self.camera.update(self.players[0])
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
 
-            for entity in [self.player, self.player2]:
-                entity.draw(self.canvas.get_canvas(), self.camera)
+            # for entity in [self.player, self.player2]:
+            #     entity.draw(self.canvas.get_canvas(), self.camera)
 
             keys = pygame.key.get_pressed()
 
             if keys[pygame.K_RIGHT]:
-                self.player.move(0)
+                self.players[0].move(0)
 
             if keys[pygame.K_LEFT]:
-                self.player.move(1)
+                self.players[0].move(1)
 
             if keys[pygame.K_UP]:
-                self.player.move(2)
+                self.players[0].move(2)
 
             # Send Network Stuff
-            self.player2.x, self.player2.y = self.parse_data(self.send_data())
+            
+            if not self.debug:
+                player_data = self.send_data()
+                other_players = self.parse_data(player_data)
+                self.players = [self.players[0]] + other_players
+            # self.player2.x, self.player2.y = self.parse_data(self.send_data())
 
             # Update Canvas
+            
             self.canvas.draw_background()
-            self.player.draw(self.canvas.get_canvas())
-            self.player2.draw(self.canvas.get_canvas())
+            for player in self.players:
+                player.draw(self.canvas.get_canvas(), self.camera)
             self.canvas.update()
+            # self.canvas.draw_background()
+            # self.player.draw(self.canvas.get_canvas(), self.camera)
+            # self.player2.draw(self.canvas.get_canvas(), self.camera)
+            # self.canvas.update()
 
         pygame.quit()
 
@@ -124,17 +152,18 @@ class Game:
 
 
 class Canvas:
-
-    def __init__(self, w, h, name="None"):
+    
+    def __init__(self, w, h, cell_size, name="None"):
         self.width = w
         self.height = h
+        self.cell_size = cell_size
         self.screen = pygame.display.set_mode((w,h))
         pygame.display.set_caption(name)
 
     @staticmethod
     def update():
         pygame.display.update()
-
+        
     def draw_text(self, text, size, x, y):
         pygame.font.init()
         font = pygame.font.SysFont("comicsans", size)
@@ -144,6 +173,11 @@ class Canvas:
 
     def get_canvas(self):
         return self.screen
-
+    
     def draw_background(self):
-        self.screen.fill((255,255,255))
+        self.screen.fill((0, 0, 0))
+        
+        for x in range(0, self.width, self.cell_size):
+            pygame.draw.line(self.screen, (255,255,255), (x,0), (x,self.height))
+        for y in range(0, self.height, self.cell_size):
+            pygame.draw.line(self.screen, (255,255,255), (0,y), (self.width,y))
--- network.py
+++ network.py
@@ -1,28 +1,26 @@
 import socket
-
+import pickle
 
 class Network:
-
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.host = '172.16.102.145'
+        self.host = '172.16.102.146'
         self.port = 1738
         self.addr = (self.host, self.port)
         self.id = self.connect()
 
     def connect(self):
         self.client.connect(self.addr)
-        return self.client.recv(2048).decode()
+        return pickle.loads(self.client.recv(2048))
 
     def send(self, data):
         """
-        :param data: str
-        :return: str
+        :param data: Object
+        :return: Object
         """
         try:
-            self.client.send(str.encode(data))
-            reply = self.client.recv(2048).decode()
+            self.client.send(pickle.dumps(data))
+            reply = pickle.loads(self.client.recv(2048))
             return reply
         except socket.error as e:
             return str(e)
-        
